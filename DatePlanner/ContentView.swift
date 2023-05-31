fe33325ec3516b4b546b33df00cfd7d5db410e96
ngalinos95 <ngalinos95@me.com>
2023-05-31 14:54:26 +0300

    Initial Commit

---
+++ DatePlanner/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DatePlanner
+//
+//  Created by Nikos Galinos on 31/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DatePlanner/DatePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePlannerApp.swift
+//  DatePlanner
+//
+//  Created by Nikos Galinos on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct DatePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
